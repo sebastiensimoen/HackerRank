b1875f63a1e43c27dca4b6b7b7d362ac76c537e8
sebastiensimoen <123328668+sebastiensimoen@users.noreply.github.com>
2023-02-02 21:59:28 +0100

    Update Day_2_Operators.py
    https://www.hackerrank.com/challenges/30-operators/problem?isFullScreen=true

--- Tutorials/30 Days of Code/Day_2_Operators.py
+++ Tutorials/30 Days of Code/Day_2_Operators.py
@@ -20,10 +20,11 @@ def solve(meal_cost, tip_percent, tax_percent):
     tip = meal_cost / 100 * tip_percent
     tax = tax_percent / 100 * meal_cost
     total_cost = meal_cost + tip + tax
-    print(round(total_cost))
+    return round(total_cost)
 
 if __name__ == '__main__':
     meal_cost = float(input().strip())
     tip_percent = int(input().strip())
     tax_percent = int(input().strip())
-    solve(meal_cost, tip_percent, tax_percent)
+    result = solve(meal_cost, tip_percent, tax_percent)
+    print(result)
