8c30f46e8369f4c6ba4154ade7903ab7cc21c55a
sebastiensimoen <123328668+sebastiensimoen@users.noreply.github.com>
2023-02-03 23:18:32 +0100

    Create Day_2_More_Dice.py
    https://www.hackerrank.com/challenges/s10-mcq-2/problem?isFullScreen=true

---
+++ Tutorials/10 Days of Statistics/Day_2_More_Dice.py
@@ -0,0 +1,19 @@
+dice1,dice2 = [x for x in range(1,7)],[x for x in range(1,7)]
+
+def cross(set1, set2):
+    result = []
+    for i in set1:
+        for j in set2:
+                result.append((i, j))
+    return result
+dice1xdice2 = cross(dice1, dice2)
+
+print(dice1xdice2)
+
+count = 0
+for x in dice1xdice2:
+    if x[0] + x[1] == 6 and x[0] != x[1]:
+        count += 1
+probability = count / len(dice1xdice2)
+
+print("{:.2f}".format(probability))
